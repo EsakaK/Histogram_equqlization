9f4f1ac60aea0f0fb9a4dba4932cf2c530dae13b
Togel Bian <Togelbian@gmail.com>
2021-05-07 11:34:55 +0800

    "任务完成"

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     HE.draw_histogram()  # 画原始图像直方图
     HE.draw_cd_f()  # 画原始图像累积分布函数
     HE.equalization() # 利用以上计算得到的累积分布函数对图像进行均衡化，得到映射函数f,并使用映射函数f对原始图像进行均衡化，得到新的图像
-
+    HE.draw_new_histogram() # 画新图像的直方图
 
